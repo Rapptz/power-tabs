9928532770d41f89a2605342dc844968a6da438b
Rapptz <rapptz@gmail.com>
2018-01-10 08:21:37 -0500

    Add assigned domains to the options page for quick removal.

--- options/options.js
+++ options/options.js
@@ -3,9 +3,9 @@ class GroupSetting {
     let data = allGroups[index];
     this.name = data.name;
     this.uuid = data.uuid;
-    this.filters = data.filters;
     this.index = index;
-    this.allGroups = allGroups
+    this.allGroups = allGroups;
+    this._hasAssignedDomain = false;
     this.buildView();
   }
 
@@ -17,48 +17,75 @@ class GroupSetting {
     browser.storage.local.set({groups: this.allGroups});
   }
 
+  saveNewName() {
+    let newText = this._nameEdit.value.trim();
+    if(newText) {
+      if(this.name !== newText) {
+        this._nameEdit.classList.remove("invalid");
+        this.data.name = newText;
+        this.name = newText;
+        this._header.textContent = `Group Settings – ${newText}`;
+        this.save();
+      }
+    }
+    else {
+      this._nameEdit.classList.add("invalid");
+    }
+  }
+
   buildView() {
     var templ = document.getElementById("group-template").content.cloneNode(true);
 
     this._header = templ.getElementById("groupHeader");
     this._header.id = "";
-    this._header.innerText = this.name;
+    this._header.textContent = `Group Settings – ${this.name}`;
 
     let nameEdit = templ.getElementById("groupName");
     nameEdit.id = "";
     nameEdit.value = this.name;
     this._nameEdit = nameEdit;
 
-    let saveButton = templ.getElementById("saveName");
-    saveButton.id = "";
+    nameEdit.addEventListener("blur", (e) => {
+      this.saveNewName();
+    });
 
-    saveButton.addEventListener("click", (e) => {
-      let newText = this._nameEdit.value.trim();
-      if(newText && this.name !== newText) {
-        this._nameEdit.classList.remove("invalid");
-        this.data.name = newText;
-        this.name = newText;
-        this._header.innerText = newText;
-        this.save();
+    nameEdit.addEventListener("keyup", (e) => {
+      e.preventDefault();
+      if(e.key == "Enter") {
+        this.saveNewName();
       }
-      else {
-        this._nameEdit.classList.add("invalid");
+    });
+
+    let assignedDomains = templ.getElementById("assignedDomains");
+    assignedDomains.id = "";
+    this._assignedDomains = assignedDomains;
+
+    assignedDomains.addEventListener("keydown", (e) => {
+      if(e.key === "Delete") {
+        let selected = [...assignedDomains.children].filter((n) => n.selected).map((n) => {
+          assignedDomains.removeChild(n);
+          return n.getAttribute("data-key");
+        });
+        browser.storage.local.remove(selected);
       }
     });
 
     this.view = templ;
   }
 
-  addView(parent) {
-    // parent.appendChild(this._header);
-    parent.appendChild(this.view);
+  addAssignment(domainName) {
+    let option = document.createElement("option");
+    option.className = "assignment";
+    option.textContent = domainName;
+    option.setAttribute("data-key", `page:${domainName}`);
+    this._assignedDomains.appendChild(option);
   }
 }
 
-async function loadSettings() {
+function loadGroupSettings(data) {
   var groupSettings = document.getElementById("group-settings");
+  var lookup = new Map();
 
-  let data = await browser.storage.local.get("groups");
   if(!data.hasOwnProperty("groups")) {
     let el = document.createElement("p");
     el.innerText = "Seems we don't have anything here...";
@@ -68,8 +95,31 @@ async function loadSettings() {
 
   for(let index = 0; index < data.groups.length; ++index) {
     let setting = new GroupSetting(data.groups, index);
-    setting.addView(groupSettings);
+    lookup.set(data.groups[index].uuid, setting);
+    groupSettings.appendChild(setting.view);
   }
+
+  // check for page assignments
+  for(let key of Object.keys(data)) {
+    if(key.indexOf("page:") !== 0) {
+      continue;
+    }
+
+    let setting = lookup.get(data[key].group);
+    if(setting) {
+      setting.addAssignment(key.slice(5));
+    }
+  }
+}
+
+function loadRegularSettings(data) {
+
+}
+
+async function loadSettings() {
+  let data = await browser.storage.local.get();
+  loadRegularSettings(data);
+  loadGroupSettings(data);
 }
 
 document.addEventListener("DOMContentLoaded", loadSettings);
